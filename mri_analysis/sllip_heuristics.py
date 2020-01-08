365532e717daa0a9ca60036e957a26fe65138bb5
juliagoolia28 <juschnei@udel.edu>
2020-01-08 13:05:30 -0500

    updated heuristics file

--- mri_analysis/sllip_heuristics.py
+++ mri_analysis/sllip_heuristics.py
@@ -23,21 +23,17 @@ def infotodict(seqinfo):
     phasediff_sl = create_key('sub-{subject}/fmap/sub-{subject}_acq-sl_run-{item:02d}_phasediff')
     mag_sl = create_key('sub-{subject}/fmap/sub-{subject}_acq-sl_run-{item:02d}_magnitude')
 
-    info = {t1:[], rest:[], asl:[], phasediff_rest:[], mag_rest:[], phasediff_sl:[], mag_sl:[]}
+    info = {t1:[], rest:[], asl:[], phasediff_rest:[], mag_rest:[]}
 
     for idx, s in enumerate(seqinfo):
-        if ((s.dim3 == 160) or (s.dim3 == 176)) and (s.dim4 == 1) and ('T1' in s.protocol_name):
+        if (s.dim3 == 352) and (s.dim4 == 1) and ('T1' in s.protocol_name):
             info[t1] = [s.series_id]
-        if (s.dim4 == 383) and ('ep2d_bold_sms_abcd' in s.protocol_name):
+        if (s.dim4 == 766) and ('ep2d_bold_sms_abcd' in s.protocol_name):
             info[rest].append({'item': s.series_id})
-        if (s.dim4 == 1) and (s.dim3 == 65) and ('gre_field_mapping_2.4mm' in s.protocol_name):
-            info[phasediff_rest].append({'item': s.series_id})
         if (s.dim4 == 1) and (s.dim3 == 130) and ('gre_field_mapping_2.4mm' in s.protocol_name):
+            info[phasediff_rest].append({'item': s.series_id})
+        if (s.dim4 == 1) and (s.dim3 == 260) and ('gre_field_mapping_2.4mm' in s.protocol_name):
             info[mag_rest].append({'item': s.series_id})
         if (s.dim4 >= 230) and (not s.is_motion_corrected) and ('asl' in s.protocol_name):
             info[asl].append({'item': s.series_id})
-        if (s.dim4 == 1) and (s.dim3 == 55) and ('gre_field_mapping_2.5mm' in s.protocol_name):
-            info[phasediff_sl].append({'item': s.series_id})
-        if (s.dim4 == 1) and (s.dim3 == 110) and ('gre_field_mapping_2.5mm' in s.protocol_name):
-            info[mag_sl].append({'item': s.series_id})
     return info
